5d6d9c22d4b5fccbc6546bed04c6e36306fb5703
cryancaire <cryancaire@gmail.com>
2015-12-21 15:53:32 -0600

    Twitch Live Multi-Streaming

---
+++ Live Mult-Streaming/index.php
@@ -0,0 +1,61 @@
+<!HTML>
+<head>
+	<title>C-Ryan's Twitch Mult-Stream</title>
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	  <script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+  </script>
+</head>
+<body bgcolor="black">
+<?
+extract($_REQUEST);
+extract($_SERVER);
+extract($_SESSION);
+
+if (!$group) {
+  $channels="cryancaire,huevos74,sobuto,chuckdatruc7";
+  $chat = "cryancaire";
+}
+if (!$h) {
+  $h = "400";
+}
+if (!$w) {
+  $w = "400";
+}
+if ($channels) {
+  $cArray = explode(",", $channels);
+}
+if (!$chat) {
+	$chat = $cArray[0];
+}
+$howManyChan = count($cArray);
+$cols = round($howManyChan/2);
+if ($howManyChan == 4) {
+  $cols = "2";
+}
+$chatW = ($howManyChan*40)+350;
+echo "<table><tr><td>";
+echo "<table><tr>";
+	$tabLink = "<div id=\"tabs\"><ul>";
+	$tabCont = "";
+for ($i=0;$i<$howManyChan;$i++){
+	$tabLink = $tabLink."<li><a href=\"#$cArray[$i]\">$cArray[$i]</a></li>";
+	$tabCont = $tabCont."<div id=\"$cArray[$i]\"><p><iframe frameborder=\"0\" scrolling=\"0\" src=\"http://twitch.tv/$cArray[$i]/chat?popout=\" height=\"800\" width=\"$chatW\"></iframe>  </p></div>";
+  echo "<td>";
+  echo "<iframe src=\"http://player.twitch.tv/?channel=$cArray[$i]\" width=\"$w\" height=\"$h\" frameborder=\"0\" scrolling=\"no\" class=\"livestream_video\"></iframe>";
+  echo "</td>";
+  		if ((($i+1)%$cols)=="0"){
+			echo "</tr><tr>";
+		}
+}
+$tabLink = $tabLink."</ul>";
+$tabCont = $tabCont."</div>";
+echo "</tr></table>";
+echo "<td rowspan=2>$tabLink $tabCont</td>";
+echo "</tr></table>";
+?>
+</body>
---
+++ Live Mult-Streaming/setup.php
@@ -0,0 +1,45 @@
+<!HTML>
+<head>
+  <style>
+    code {
+      background-color: #F6E3CE;
+      margin-left: 20px;
+    }
+  </style>
+  <title>C-Ryan's Twitch Mult-Stream Setup</title>
+  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <script>
+  $(document).ready(function(){
+      $( "#clickButton" ).click(function() {
+        var theURL = "http://coreycaire.com/livestream/?group=custom&channels="+$('#streams').val()+"&h="+$('#sHeight').val()+"&w="+$('#sWidth').val();
+        $("#resPan").html("<input type=text value="+theURL+" size=100>");
+        window.open(theURL);
+      })
+		});
+  </script>
+  
+</head>
+
+<H1>
+  C-Ryan's Twitch Mult-Stream Setup
+</H1>
+
+<table>
+<tr><td>
+Enter the twitch usernames here: <input type=text id="streams" size=40><br>
+Enter multiple by seperating with a comma.<br>
+Twitch username is as follows: <code>http://twitch.tv/<i>username</i></code>
+  </td></tr> 
+<tr><td>
+<br>Stream Width: <input type=text id="sWidth" size=4 value="400"> <br>
+Stream Height: <input type=text id="sHeight" size=4 value="400"><br>
+All streams will be this size.
+</td></tr>
+<tr><td><button id="clickButton">
+  Go!
+  </button></td></tr>
+</table>
+<Br>
+  <div id="resPan">
+    
+  </div>
